d014bd926f2ed9e2aa149b243e6f0312f08897c6
monodyle <monodylh@gmail.com>
2022-04-13 19:51:55 +0700

    feat: remove sideway heading (not support mobile)

--- components/heading/sideway.tsx
+++
@@ -1,9 +0,0 @@
-import s from "./heading.module.css";
-
-interface Props {
-  children: React.ReactNode;
-}
-
-export const SidewayHeading = (props: Props) => (
-  <h2 className={s.sideway}>{props.children}</h2>
-);
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,23 +2,17 @@ import { Mission } from "~app/about/mission";
 import { Team } from "~app/about/team";
 import { Layout } from "~app/layout";
 import { SEO } from "~app/layout/seo";
-import { SidewayHeading } from "~components/heading/sideway";
 import { PAGES } from "~constants";
 
 export default function AboutPage() {
   return (
     <Layout>
       <SEO title={PAGES.ABOUT.title} tailTitle />
-      <div className="max-w-5xl px-12 py-16 pr-0 mx-auto lg:pr-12">
-        <div className="flex items-start">
-          <SidewayHeading>About</SidewayHeading>
-          <div className="flex-1 px-12">
-            <p className="mb-6 font-serif text-2xl text-right">
-              We build tools that make blockchain easier than ever for everyone.
-            </p>
-            <Mission />
-          </div>
-        </div>
+      <div className="max-w-5xl px-12 py-16 pr-0 mx-auto lg:pr-12 md:mt-12">
+        <p className="mb-6 font-serif text-2xl text-right">
+          We build tools that make blockchain easier than ever for everyone.
+        </p>
+        <Mission />
         <Team />
       </div>
     </Layout>
