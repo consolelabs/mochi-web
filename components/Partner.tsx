d46cc1af18b28a4e454c4107dfd8fa5df52be4b2
tuanddd <vincenttuandao@gmail.com>
2023-02-20 16:38:34 +0700

    chore: add chains and partners

---
+++ components/Partner.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+import clsx from 'classnames'
+
+const Image = (props: any) => (
+  <img
+    {...props}
+    className={clsx(
+      props.className,
+      'grayscale hover:grayscale-0 duration-100 ease-in-out mx-7 md:mx-10',
+    )}
+    style={{
+      transitionProperty: 'filter',
+    }}
+    alt={props.alt}
+  />
+)
+
+export const Partner = () => {
+  return (
+    <Marquee className="w-20 h-20 py-3 overflow-hidden flex" pauseOnHover>
+      <Image
+        className="h-full"
+        src="/partner/blue-move.png"
+        alt="blue-move-logo"
+      />
+      <Image className="h-full" src="/partner/okx.png" alt="okx-logo" />
+      <Image className="h-full" src="/partner/onus.webp" alt="onus-logo" />
+      <Image
+        className="h-full"
+        src="/partner/solscan.webp"
+        alt="solscan-logo"
+      />
+    </Marquee>
+  )
+}
