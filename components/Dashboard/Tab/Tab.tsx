df601ea8c6b092c6dd105088eba995aa31c0309e
tuanddd <vincenttuandao@gmail.com>
2023-03-15 11:46:25 +0700

    feat: tab

---
+++ components/Dashboard/Tab/Tab.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Tab as HeadlessTab } from '@headlessui/react'
+import { tab } from './styles'
+import clsx from 'clsx'
+
+type Props = {
+  headings: string[]
+  children?: React.ReactNode
+}
+
+export const Tab = ({ children, headings }: Props) => {
+  return (
+    <div className={tab({})}>
+      <HeadlessTab.Group>
+        <HeadlessTab.List className="flex gap-x-8">
+          {headings.map((h, i) => {
+            return (
+              <HeadlessTab className="outline-none" key={`tab-${h}-${i}`}>
+                {({ selected }) => {
+                  return (
+                    <span
+                      className={clsx(
+                        'font-medium pb-1 border-b-2 transition-colors duration-100 ease-in',
+                        {
+                          'text-foreground border-b-foreground': selected,
+                          'text-[#7A7E85] border-b-transparent': !selected,
+                        },
+                      )}
+                    >
+                      {h}
+                    </span>
+                  )
+                }}
+              </HeadlessTab>
+            )
+          })}
+        </HeadlessTab.List>
+        <HeadlessTab.Panels>{children}</HeadlessTab.Panels>
+      </HeadlessTab.Group>
+    </div>
+  )
+}
+
+Tab.Panel = HeadlessTab.Panel
