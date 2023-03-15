1aeb857377e5a7dc968a3139397821a0ee52e75c
tuanddd <vincenttuandao@gmail.com>
2023-03-15 11:48:10 +0700

    fix: circled icon z index

--- components/CircledIcon.tsx
+++ components/CircledIcon.tsx
@@ -10,7 +10,13 @@ export const CircledIcon = (props: Props) => {
   return (
     <div className="flex relative justify-center items-center p-4 w-16 h-16">
       <Image fill src={iconContainer} alt="" />
-      <Image width={40} height={40} src={props.src} alt="" />
+      <Image
+        width={40}
+        height={40}
+        src={props.src}
+        alt=""
+        className="relative"
+      />
     </div>
   )
 }
