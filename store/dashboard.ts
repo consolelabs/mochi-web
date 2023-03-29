07be81dd6daf0f59665370fd11637bbd2392aff3
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2023-03-29 14:52:35 +0700

    feat: final update to top member listing (#60)
    * feat: final update to top member listing
    
    * feat: update
    
    * chore: tweak abit
    
    ---------
    
    Co-authored-by: tuanddd <vincenttuandao@gmail.com>

--- store/dashboard.ts
+++ store/dashboard.ts
@@ -3,36 +3,58 @@ import { API, GET_PATHS } from '~constants/api'
 import { Response } from '~types/api'
 import { ViewUserDiscordGuilds } from '~types/mochi-profile-schema'
 import {
-  DiscordgoMember,
+  ResponseDiscordGuildRole,
   ResponseGetGuildResponse,
   ResponseTopUser,
 } from '~types/mochi-schema'
 
-type State = {
-  getServerList: () => Promise<Response<ViewUserDiscordGuilds>>
-  // TODO: Use official interfaces from swagger schema of Mochi API
-  server?: ResponseGetGuildResponse
-  getServer: (id: string) => Promise<void>
-  getServerMemberList: (query: any) => Promise<Response<ResponseTopUser>>
+type Pagination = {
+  page?: number
+  limit?: number
 }
 
-export const useDashboardStore = create<State>((set, get) => ({
+type Query = {
+  sort?: string
+  query?: string
+}
+
+type State = {
+  getServerList: () => Promise<Response<ViewUserDiscordGuilds>>
+  server?: ResponseGetGuildResponse
+  roles?: ResponseDiscordGuildRole[]
+  getServer: (id: string) => void
+  getServerMemberList: (
+    query: Query &
+      Pagination & { guild_id?: string; user_id?: string; platform?: string },
+  ) => Promise<Response<ResponseTopUser>>
+}
+
+export const useDashboardStore = create<State>((set) => ({
   getServerList: () => {
     return API.MOCHI_PROFILE.get(GET_PATHS.GUILDS).res((res) => res.json())
   },
 
   server: undefined,
+  roles: undefined,
   getServer: (id: string) => {
-    return API.MOCHI.get(GET_PATHS.GUILD(id))
+    API.MOCHI.get(GET_PATHS.GUILD(id))
       .res((res) => res.json())
       .then((res) => {
         set(() => ({
           server: res,
         }))
       })
+
+    API.MOCHI.get(GET_PATHS.GUILD_ROLES(id))
+      .res((res) => res.json())
+      .then((res) => {
+        set(() => ({
+          roles: res.data,
+        }))
+      })
   },
 
-  getServerMemberList: (query: any) => {
+  getServerMemberList: (query) => {
     return API.MOCHI.query(query)
       .get(GET_PATHS.USERS_TOP)
       .res((res) => res.json())
