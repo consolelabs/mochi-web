3becf08af1281db15dac66fe7dc02dccdf98b17a
minhlq <minhlq0696@gmail.com>
2023-08-22 07:41:22 +0700

    feat: update api intergration link and footer link (#79)

--- components/Home/ApiIntegration.tsx
+++ components/Home/ApiIntegration.tsx
@@ -1,4 +1,13 @@
+import { useClipboard } from '@dwarvesf/react-hooks'
+import { Icon } from '@iconify/react'
+
+const codeString = `
+  curl --request GET \\\n --url https://api.mochi-pay.console.so/api/\nv1/profile/0/monthly-stats \\\n --header 'accept: application/json'
+  `
+
 export default function ApiIntegration() {
+  const { hasCopied, onCopy } = useClipboard(codeString)
+
   return (
     <section className="bg-[#26272B] py-16 md:py-24">
       <div className="container mx-auto max-w-5xl px-6">
@@ -18,12 +27,30 @@ export default function ApiIntegration() {
               </a>
             </div>
           </div>
-          <div>
-            <img
-              src="/home/api.png"
-              alt="api integration"
-              className="w-full flex-1"
-            />
+          <div className="bg-black px-8 py-8 rounded-3xl relative leading-7">
+            <span className="px-4 uppercase text-gray-500">curl request</span>
+            <pre className="bg-black">
+              <code>
+                <span className="text-gray-400">{codeString}</span>
+              </code>
+            </pre>
+            <button
+              onClick={onCopy}
+              className="p-1 rounded-lg bg-slate-500 absolute top-6 right-6"
+              type="button"
+            >
+              {hasCopied ? (
+                <Icon
+                  className="w-4 h-4 text-green-300"
+                  icon="heroicons:check"
+                />
+              ) : (
+                <Icon
+                  className="w-4 h-4 text-white"
+                  icon="heroicons:clipboard"
+                />
+              )}
+            </button>
           </div>
         </div>
       </div>
