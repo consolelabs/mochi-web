56c85678008fde3590f194679b8545dea4b37f38
tuanddd <vincenttuandao@gmail.com>
2023-09-15 10:50:13 +0700

    feat: show dynamic template in tx

--- pages/tx/[id].tsx
+++ pages/tx/[id].tsx
@@ -17,6 +17,39 @@ import {
 } from '~utils/image'
 import clsx from 'clsx'
 import { UI } from '~constants/mochi'
+import { hpbd, appreciation, achievement, wedding } from 'utils/image'
+import Image from 'next/image'
+
+type TemplateName =
+  | 'happy_birthday'
+  | 'appreciation'
+  | 'achievement'
+  | 'anniversary'
+
+type Template = { img: string; phrase: string; title: string }
+
+const templates: Record<TemplateName, Template> = {
+  happy_birthday: {
+    phrase: 'sent a birthday gift',
+    img: hpbd.src,
+    title: 'Happy Birthday ⎯ Tip from <user> ⎯ Mochi',
+  },
+  appreciation: {
+    phrase: 'sent a thank you gift',
+    img: appreciation.src,
+    title: 'Thank You ⎯ Tip from <user> ⎯ Mochi',
+  },
+  achievement: {
+    phrase: 'sent a gift to celebrate the achievement',
+    img: achievement.src,
+    title: 'Celebrate Achievement ⎯ Tip from <user> ⎯ Mochi',
+  },
+  anniversary: {
+    phrase: 'sent an anniversary gift',
+    img: wedding.src,
+    title: 'Happy Anniversary ⎯ Tip from <user> ⎯ Mochi',
+  },
+}
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { id } = ctx.query
@@ -37,6 +70,10 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     }
   }
 
+  const templateName =
+    transfer.metadata.template.slug.toLowerCase() as TemplateName
+  const template = templates[templateName]
+
   const type = transfer.type
 
   let avatar = transfer.from_profile.avatar
@@ -45,6 +82,10 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     transfer.from_profile_id,
     transfer.other_profile_id,
   )
+
+  if (sender?.plain) {
+    template.title.replace('<user>', sender.plain)
+  }
 
   if (type === 'in') {
     ;[sender, receiver] = [receiver, sender]
@@ -98,6 +139,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     external_id: transfer.external_id,
     moniker: transfer.metadata.moniker || '',
     original_amount: transfer.metadata.original_amount || '',
+    template,
   }
 
   if (transfer) {
@@ -117,6 +159,41 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       tokenIcon,
     },
   }
+}
+
+function Template({
+  title,
+  phrase,
+  img,
+  ...rest
+}: Template & { platformIcon?: string; avatar: string }) {
+  return (
+    <div className="relative w-full bg-inherit h-[200px]">
+      <Image
+        src={img}
+        fill
+        className="object-cover object-bottom"
+        alt={title}
+      />
+      <div className="absolute bottom-0 left-1/2 p-0.5 rounded-full -translate-x-1/2 translate-y-1/2 bg-inherit">
+        <Avatar {...rest} />
+      </div>
+    </div>
+  )
+}
+
+function Avatar({
+  platformIcon,
+  avatar,
+}: {
+  platformIcon?: string
+  avatar: string
+}) {
+  return !platformIcon ? (
+    <img className="w-14 h-14 rounded-full" src={avatar} alt="" />
+  ) : (
+    <CutoutAvatar size="sm" src={avatar} cutoutSrc={platformIcon} />
+  )
 }
 
 export default function Transfer({
@@ -160,164 +237,170 @@ export default function Transfer({
       skipAuth
       childSEO={
         <SEO
-          title={`Tip from ${sender.value} - Mochi`}
+          title={
+            ogData.template
+              ? ogData.template.title
+              : `Tip from ${sender.value} - Mochi`
+          }
           image={`${HOME_URL}/api/transfer-og?data=${encodeURIComponent(
             JSON.stringify(ogData),
           )}`}
-          description={`${
-            sender.value
-          } paid ${receiver} ${amountDisplay} ${unitCurrency}${
-            transfer.metadata.message
+          description={`${sender.value
+            } paid ${receiver} ${amountDisplay} ${unitCurrency}${transfer.metadata.message
               ? ` with message: "${transfer.metadata.message}"`
               : ''
-          }`}
+            }`}
           url={`${HOME_URL}/transfer/${transfer.external_id}`}
         />
       }
     >
       <div className="px-4 mx-auto w-full max-w-md font-sans drop-shadow-md">
-        <div className="flex overflow-hidden relative flex-col gap-y-12 py-3 px-6 pb-6 w-full text-center bg-white rounded-lg md:px-8 receipt">
-          <div
-            className="w-1/2 -translate-x-1/2"
-            style={{
-              display: 'flex',
-              top: 10,
-              left: '50%',
-              position: 'absolute',
-              height: 200,
-              background:
-                'linear-gradient(0deg, #f4c4c2 0%, #eec3fd 48.96%, #8fc6e4 100%)',
-              filter: 'blur(60px)',
-              opacity: 0.7,
-            }}
-          />
-          <Icon
-            icon="solar:gift-linear"
-            className="relative mt-2 w-full h-32 text-gray-700 drop-shadow-2xl"
-          />
-          <div className="flex relative flex-col items-center">
-            {!platformIcon ? (
-              <img
-                className="w-14 h-14 rounded-full"
-                src={sender.avatar}
-                alt=""
-              />
-            ) : (
-              <CutoutAvatar
-                size="sm"
-                src={sender.avatar}
-                cutoutSrc={platformIcon}
-              />
-            )}
-            <div className="mt-2 text-sm">
-              <span className="font-medium">{sender.value}</span>
-              <br />
-              <span className="text-xs font-light text-gray-500">sent</span>
-            </div>
-            <div className="flex justify-center items-center mt-8 font-medium">
+        <div className="flex overflow-hidden relative flex-col gap-y-6 w-full text-center bg-white rounded receipt">
+          {ogData.template ? (
+            <Template
+              {...ogData.template}
+              platformIcon={platformIcon}
+              avatar={sender.avatar}
+            />
+          ) : (
+            <>
               <div
-                className={clsx('flex', {
-                  'flex-col': isLongNumber,
-                  'items-center': isLongNumber,
-                  'items-baseline': !isLongNumber,
-                })}
-              >
-                <img
-                  className={clsx('mr-1 w-7 h-7', {
-                    'inline-block': !isLongNumber,
-                    hidden: isLongNumber,
-                  })}
-                  src={tokenIcon || coinIcon.src}
-                  alt=""
-                />
-                <div className="text-5xl">{amountDisplay}</div>
+                className="w-1/2 -translate-x-1/2"
+                style={{
+                  display: 'flex',
+                  top: 10,
+                  left: '50%',
+                  position: 'absolute',
+                  height: 200,
+                  background:
+                    'linear-gradient(0deg, #f4c4c2 0%, #eec3fd 48.96%, #8fc6e4 100%)',
+                  filter: 'blur(60px)',
+                  opacity: 0.7,
+                }}
+              />
+              <Icon
+                icon="solar:gift-linear"
+                className="relative mt-2 w-full h-32 text-gray-700 drop-shadow-2xl"
+              />
+            </>
+          )}
+          <div className="flex flex-col gap-y-12 py-3 px-6 pb-6 md:px-8">
+            <div className="flex relative flex-col items-center">
+              {ogData.template ? null : (
+                <Avatar platformIcon={platformIcon} avatar={sender.avatar} />
+              )}
+              <div className="mt-2 text-sm">
+                <span className="font-medium">{sender.value}</span>
+                <br />
+                <span className="text-xs font-light text-gray-500">
+                  {ogData.template ? ogData.template.phrase : 'sent'}
+                </span>
+              </div>
+              <div className="flex justify-center items-center mt-8 font-medium">
                 <div
-                  className={clsx('flex mt-1', {
+                  className={clsx('flex', {
+                    'flex-col': isLongNumber,
                     'items-center': isLongNumber,
-                    'items-baseline ml-1': !isLongNumber,
+                    'items-baseline': !isLongNumber,
                   })}
                 >
                   <img
                     className={clsx('mr-1 w-7 h-7', {
-                      'inline-block': isLongNumber,
-                      hidden: !isLongNumber,
+                      'inline-block': !isLongNumber,
+                      hidden: isLongNumber,
                     })}
-                    src={tokenIcon ?? coinIcon.src}
+                    src={tokenIcon || coinIcon.src}
                     alt=""
                   />
-                  <div className="text-4xl">{unitCurrency}</div>
+                  <div className="text-5xl">{amountDisplay}</div>
+                  <div
+                    className={clsx('flex mt-1', {
+                      'items-center': isLongNumber,
+                      'items-baseline ml-1': !isLongNumber,
+                    })}
+                  >
+                    <img
+                      className={clsx('mr-1 w-7 h-7', {
+                        'inline-block': isLongNumber,
+                        hidden: !isLongNumber,
+                      })}
+                      src={tokenIcon ?? coinIcon.src}
+                      alt=""
+                    />
+                    <div className="text-4xl">{unitCurrency}</div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <span className="text-xl">
-              {amountApproxMoniker} &asymp;{' '}
-              {mochiUtils.formatUsdDigit(transfer.usd_amount)}
-            </span>
-          </div>
-          {transfer.metadata.message && (
-            <span className="relative mt-3 font-normal text-center">
-              &ldquo;{transfer.metadata.message}&rdquo;
-            </span>
-          )}
-          <div className="relative -mx-6 text-gray-400">
-            <div className="flex relative flex-col gap-y-2 gap-x-4 py-4 font-mono xs:flex-row">
-              <div className="absolute top-0 left-0 w-full h-full scale-x-125 receipt-dashed-box" />
-              <ul className="relative flex-1 px-2 space-y-2 text-xs">
-                <li className="flex gap-x-3 justify-between">
-                  <span className="font-normal text-current">From</span>
-                  <span className="font-normal text-current">
-                    {sender.value}
-                  </span>
-                </li>
-                <li className="flex gap-x-3 justify-between">
-                  <span className="font-normal text-current">To</span>
-                  <span className="font-normal text-current">{receiver}</span>
-                </li>
-
-                <li className="flex gap-x-3 justify-between">
-                  <span className="font-normal text-current">Amount</span>
-                  <span className="font-normal text-current">
-                    {amountSection}
-                    <span className="ml-1 font-normal text-current">
-                      {unitAmountSection}
-                    </span>
-                  </span>
-                </li>
-              </ul>
-              <ul className="relative flex-1 px-2 space-y-2 text-xs">
-                <li className="flex justify-between">
-                  <span className="font-normal text-current">Tx ID</span>
-                  <span className="font-normal text-current">
-                    {transfer.external_id}
-                  </span>
-                </li>
-                <li className="flex justify-between">
-                  <span className="font-normal text-current">Date</span>
-                  <span className="font-normal text-current">
-                    {format(
-                      new Date(transfer.created_at),
-                      'dd/MM/yyyy hh:mmaa',
-                    )}
-                  </span>
-                </li>
-                <li className="flex justify-between">
-                  <span className="font-normal text-current">Status</span>
-                  <span className="font-normal text-current">Success</span>
-                </li>
-                <img
-                  src={successStampIcon.src}
-                  className="absolute right-0 top-1/2 flex-shrink-0 h-full opacity-30 scale-[2]"
-                  alt=""
-                />
-              </ul>
-            </div>
-            <div className="flex justify-between px-2 mt-5 text-xs font-light">
-              <a className="text-current" href="#">
-                Mochi &copy; 2023
-              </a>
-              <span className="text-current">
-                {format(new Date(transfer.created_at), 'dd/MM/yyyy hh:mmaa')}
+              <span className="text-xl">
+                {amountApproxMoniker} &asymp;{' '}
+                {mochiUtils.formatUsdDigit(transfer.usd_amount)}
               </span>
+            </div>
+            {transfer.metadata.message && (
+              <span className="relative mt-3 font-normal text-center">
+                &ldquo;{transfer.metadata.message}&rdquo;
+              </span>
+            )}
+            <div className="relative -mx-6 text-gray-400">
+              <div className="flex relative flex-col gap-y-2 gap-x-4 py-4 font-mono xs:flex-row">
+                <div className="absolute top-0 left-0 w-full h-full scale-x-125 receipt-dashed-box" />
+                <ul className="relative flex-1 px-2 space-y-2 text-xs">
+                  <li className="flex gap-x-3 justify-between">
+                    <span className="font-normal text-current">From</span>
+                    <span className="font-normal text-current">
+                      {sender.value}
+                    </span>
+                  </li>
+                  <li className="flex gap-x-3 justify-between">
+                    <span className="font-normal text-current">To</span>
+                    <span className="font-normal text-current">{receiver}</span>
+                  </li>
+
+                  <li className="flex gap-x-3 justify-between">
+                    <span className="font-normal text-current">Amount</span>
+                    <span className="font-normal text-current">
+                      {amountSection}
+                      <span className="ml-1 font-normal text-current">
+                        {unitAmountSection}
+                      </span>
+                    </span>
+                  </li>
+                </ul>
+                <ul className="relative flex-1 px-2 space-y-2 text-xs">
+                  <li className="flex justify-between">
+                    <span className="font-normal text-current">Tx ID</span>
+                    <span className="font-normal text-current">
+                      {transfer.external_id}
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="font-normal text-current">Date</span>
+                    <span className="font-normal text-current">
+                      {format(
+                        new Date(transfer.created_at),
+                        'dd/MM/yyyy hh:mmaa',
+                      )}
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="font-normal text-current">Status</span>
+                    <span className="font-normal text-current">Success</span>
+                  </li>
+                  <img
+                    src={successStampIcon.src}
+                    className="absolute right-0 top-1/2 flex-shrink-0 h-full opacity-30 scale-[2]"
+                    alt=""
+                  />
+                </ul>
+              </div>
+              <div className="flex justify-between px-2 mt-5 text-xs font-light">
+                <a className="text-current" href="#">
+                  Mochi &copy; 2023
+                </a>
+                <span className="text-current">
+                  {format(new Date(transfer.created_at), 'dd/MM/yyyy hh:mmaa')}
+                </span>
+              </div>
             </div>
           </div>
         </div>
--- utils/image.ts
+++ utils/image.ts
@@ -36,3 +36,8 @@ export { default as failStampIcon } from '../public/assets/fail-stamp.png'
 export { default as discordLogo } from '../public/platforms/discord.png'
 export { default as telegramLogo } from '../public/platforms/telegram.webp'
 export { default as xlogo } from '../public/platforms/x.png'
+
+export { default as hpbd } from '../public/assets/tx/hpbd.png'
+export { default as appreciation } from '../public/assets/tx/appreciation.png'
+export { default as wedding } from '../public/assets/tx/wedding.png'
+export { default as achievement } from '../public/assets/tx/achievement.png'
