80142f701135865a995cfa9aaa19161456856046
tuanddd <vincenttuandao@gmail.com>
2023-10-09 14:34:30 +0700

    fix: login telegram

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,7 @@ import { isBeta } from '~constants'
 import { button } from '~components/button'
 import Script from 'next/script'
 import { AUTH_TELEGRAM_USERNAME, MOCHI_PROFILE_API } from '~envs'
+import { isSSR } from '@dwarvesf/react-utils'
 
 const TopProgressBar = dynamic(() => import('~app/layout/nprogress'), {
   ssr: false,
@@ -114,6 +115,8 @@ export default function App(props: AppPropsWithLayout) {
     }
   }, [onOpen])
 
+  const urlLocation = isSSR() ? '/' : window.location.href
+
   return (
     <StrictMode>
       <Toaster
@@ -129,7 +132,7 @@ export default function App(props: AppPropsWithLayout) {
           async
           src="https://telegram.org/js/telegram-widget.js?22"
           data-telegram-login={AUTH_TELEGRAM_USERNAME}
-          data-auth-url={`${MOCHI_PROFILE_API}/profiles/auth/telegram?url_location=${window.location.href}`}
+          data-auth-url={`${MOCHI_PROFILE_API}/profiles/auth/telegram?url_location=${urlLocation}`}
           data-size="large"
           data-request-access="write"
         ></Script>
